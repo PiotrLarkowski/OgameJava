7f8f90b23a601430681621b35a654ad859845ad5
Piotr Larkowski <p.larkowski90@gmail.com>
2025-03-11 03:35:40 +0100

    Create button selection by keyboard

--- src/KeyHandler.java
+++ src/KeyHandler.java
@@ -9,14 +9,26 @@ public class KeyHandler implements KeyListener {
 
     @Override
     public void keyPressed(KeyEvent e) {
-        if(e.getKeyCode() == KeyEvent.VK_W){
-            System.out.println("W");
-        }else if(e.getKeyCode() == KeyEvent.VK_S) {
-            System.out.println("S");
-        }else if(e.getKeyCode() == KeyEvent.VK_A) {
-            System.out.println("A");
-        }else if(e.getKeyCode() == KeyEvent.VK_D) {
-            System.out.println("D");
+        if(e.getKeyCode() == KeyEvent.VK_1){
+            MiddleGamePanel.actualWindow = 1;
+        }else if(e.getKeyCode() == KeyEvent.VK_2) {
+            MiddleGamePanel.actualWindow = 2;
+        }else if(e.getKeyCode() == KeyEvent.VK_3) {
+            MiddleGamePanel.actualWindow = 3;
+        }else if(e.getKeyCode() == KeyEvent.VK_4) {
+            MiddleGamePanel.actualWindow = 4;
+        }else if(e.getKeyCode() == KeyEvent.VK_5) {
+            MiddleGamePanel.actualWindow = 5;
+        }else if(e.getKeyCode() == KeyEvent.VK_6) {
+            MiddleGamePanel.actualWindow = 6;
+        }else if(e.getKeyCode() == KeyEvent.VK_7) {
+            MiddleGamePanel.actualWindow = 7;
+        }else if(e.getKeyCode() == KeyEvent.VK_8) {
+            MiddleGamePanel.actualWindow = 8;
+        }else if(e.getKeyCode() == KeyEvent.VK_9) {
+            MiddleGamePanel.actualWindow = 9;
+        }else if(e.getKeyCode() == KeyEvent.VK_0) {
+            MiddleGamePanel.actualWindow = 0;
         }else if(e.getKeyCode() == KeyEvent.VK_ESCAPE) {
             System.exit(0);
         }
--- src/MiddleGamePanel.java
+++ src/MiddleGamePanel.java
@@ -10,6 +10,12 @@ public class MiddleGamePanel extends JPanel implements Runnable{
     final int FPS = 60;
     KeyHandler keyHandler = new KeyHandler();
 
+    int xMainWindowValue = ((int)WIDTH*20)/100;
+    int yMainWindowValue = ((int)HEIGHT)/100;
+    int widthMainWindow = ((int)WIDTH*60)/100;
+    int heightMainWindow = ((int)HEIGHT*58)/100;
+    public static int actualWindow = 1;
+
     public MiddleGamePanel() {
 
         this.setPreferredSize(new Dimension((int)WIDTH, ((int)HEIGHT*60)/100));
@@ -62,10 +68,79 @@ public class MiddleGamePanel extends JPanel implements Runnable{
     public void paintComponent(Graphics g) {
         super.paintComponent(g);
         Graphics2D g2 = (Graphics2D) g;
+
+        g2.setColor(Color.BLACK);
+
+        g2.drawRoundRect(xMainWindowValue,yMainWindowValue,widthMainWindow,heightMainWindow,20,20);
+        g2.setFont(new Font("Tahoma",Font.ITALIC,36));
+        switch(actualWindow){
+            case 1:
+                drawWindow1(g2);
+                break;
+            case 2:
+                drawWindow2(g2);
+                break;
+            case 3:
+                drawWindow3(g2);
+                break;
+            case 4:
+                drawWindow4(g2);
+                break;
+            case 5:
+                drawWindow5(g2);
+                break;
+            case 6:
+                drawWindow6(g2);
+                break;
+            case 7:
+                drawWindow7(g2);
+                break;
+            case 8:
+                drawWindow8(g2);
+                break;
+            case 9:
+                drawWindow9(g2);
+                break;
+            case 0:
+                drawWindow0(g2);
+                break;
+        }
+        createButtons(g2);
+    }
+
+    public void drawWindow0(Graphics2D g2){
+        g2.drawString("SOJUSZ",widthMainWindow/2,heightMainWindow/2);
+    }
+    public void drawWindow1(Graphics2D g2){
+        g2.drawString("PODGLĄD",widthMainWindow/2,heightMainWindow/2);
+    }
+    public void drawWindow2(Graphics2D g2){
+        g2.drawString("SUROWCE",widthMainWindow/2,heightMainWindow/2);
+    }
+    public void drawWindow3(Graphics2D g2){
+        g2.drawString("STACJA",widthMainWindow/2,heightMainWindow/2);
+    }
+    public void drawWindow4(Graphics2D g2){
+        g2.drawString("HANDLARZ",widthMainWindow/2,heightMainWindow/2);
+    }
+    public void drawWindow5(Graphics2D g2){
+        g2.drawString("BADANIA",widthMainWindow/2,heightMainWindow/2);
+    }
+    public void drawWindow6(Graphics2D g2){
+        g2.drawString("STOCZNIA",widthMainWindow/2,heightMainWindow/2);
+    }
+    public void drawWindow7(Graphics2D g2){
+        g2.drawString("OBRONA",widthMainWindow/2,heightMainWindow/2);
+    }
+    public void drawWindow8(Graphics2D g2){
+        g2.drawString("FLOTA",widthMainWindow/2,heightMainWindow/2);
+    }
+    public void drawWindow9(Graphics2D g2){
+        g2.drawString("GALAKTYKA",widthMainWindow/2,heightMainWindow/2);
+    }
+    public void createButtons(Graphics2D g2){
         int buttonsXValue = (((int)WIDTH*10)/100)-20;
         int buttonsYValue = ((int)HEIGHT)/100;
-        g2.setColor(Color.BLACK);
-        g2.drawRoundRect(((int)WIDTH*20)/100,((int)HEIGHT)/100,((int)WIDTH*60)/100,((int)HEIGHT*58)/100,20,20);
 
         g2.drawRoundRect(buttonsXValue,buttonsYValue+10,150,30,20,20);
         g2.drawRoundRect(buttonsXValue,buttonsYValue+60,150,30,20,20);
@@ -79,17 +154,15 @@ public class MiddleGamePanel extends JPanel implements Runnable{
         g2.drawRoundRect(buttonsXValue,buttonsYValue+460,150,30,20,20);
 
         g2.setFont(new Font("Tahoma",Font.ITALIC,14));
-        g2.drawString("Podgląd",buttonsXValue+50,buttonsYValue+30);
-        g2.drawString("Surowce",buttonsXValue+50,buttonsYValue+80);
-        g2.drawString("Stacja",buttonsXValue+50,buttonsYValue+130);
-        g2.drawString("Handlarz",buttonsXValue+50,buttonsYValue+180);
-        g2.drawString("Badania",buttonsXValue+50,buttonsYValue+230);
-        g2.drawString("Stocznia",buttonsXValue+50,buttonsYValue+280);
-        g2.drawString("Obrona",buttonsXValue+50,buttonsYValue+330);
-        g2.drawString("Flota",buttonsXValue+50,buttonsYValue+380);
-        g2.drawString("Galaktyka",buttonsXValue+50,buttonsYValue+430);
-        g2.drawString("Sojusz",buttonsXValue+50,buttonsYValue+480);
-        g2.drawString("Sklep",buttonsXValue+50,buttonsYValue+530);
-
+        g2.drawString("1 - Podgląd",buttonsXValue+50,buttonsYValue+30);
+        g2.drawString("2 - Surowce",buttonsXValue+50,buttonsYValue+80);
+        g2.drawString("3 - Stacja",buttonsXValue+50,buttonsYValue+130);
+        g2.drawString("4 - Handlarz",buttonsXValue+50,buttonsYValue+180);
+        g2.drawString("5 - Badania",buttonsXValue+50,buttonsYValue+230);
+        g2.drawString("6 - Stocznia",buttonsXValue+50,buttonsYValue+280);
+        g2.drawString("7 - Obrona",buttonsXValue+50,buttonsYValue+330);
+        g2.drawString("8 - Flota",buttonsXValue+50,buttonsYValue+380);
+        g2.drawString("9 - Galaktyka",buttonsXValue+50,buttonsYValue+430);
+        g2.drawString("0 - Sojusz",buttonsXValue+50,buttonsYValue+480);
     }
 }
